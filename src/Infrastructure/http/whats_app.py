005a7eb07a3bee13951a7a7dba8f6a88ff1bedc6
Samuel <samuelviniciusoficial@gmail.com>
2025-03-18 20:19:59 -0300

    adicionando o env para proteger o token

--- src/Infrastructure/http/whats_app.py
+++ src/Infrastructure/http/whats_app.py
@@ -1,15 +1,17 @@
 import random
 import os
 from twilio.rest import Client
+from dotenv import load_dotenv
 
-ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID', 'AC8bb321d902020f96a7ff7d2c3dd891d4')
-AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN', '367d16f039ef093cb6b474142c258356')
+load_dotenv()  
+twilio_sid = os.getenv('TWILIO_ACCOUNT_SID')
+twilio_auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_WHATSAPP_NUMBER = 'whatsapp:+14155238886'
 DESTINATION_WHATSAPP_NUMBER = 'whatsapp:+5511947950771'
 
 def sendMessage(code):
     try:
-        client = Client(ACCOUNT_SID, AUTH_TOKEN)
+        client = Client(twilio_sid , twilio_auth_token)
 
         message = client.messages.create(
             from_='whatsapp:+14155238886',
