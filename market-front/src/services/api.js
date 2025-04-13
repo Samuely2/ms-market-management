00a13e3b60094b9903a4eb282ce63e11fb6d5f46
Samuely2 <Samuelviniciusoficial@gmail.com>
2025-04-13 18:28:44 -0300

    configuracao cors back e front

--- market-front/src/services/api.js
+++ market-front/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
+    baseURL: 'http://localhost:5000/api', 
+    withCredentials: true /
+  });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
